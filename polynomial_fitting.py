9230e3a7e1c71c7b5758f9c0fb29faec706d159f
Elliot English <eenglish@gmail.com>
2025-02-07 15:22:04 -0800

    add monotonic check for polynomial fitting

--- polynomial_fitting.py
+++ polynomial_fitting.py
@@ -11,37 +11,50 @@ def fit_polynomial_monotonic():
   x,a,b,c,d,T0,T1=sympy.symbols("x a b c d T0 T1")
   p0,dp0,p1,dp1=sympy.symbols("p0 dp0 p1 dp1")
 
-  y=a*x**3+b*x**2+c*x+d
+  y=a+b*x+c*x**2+d*x**3
   dy=sympy.diff(y,x)
-
-  print(y.subs(x,0))
 
   system=[
     sympy.Eq(y.subs(x,T0),p0),
     sympy.Eq(dy.subs(x,T0),dp0),
     sympy.Eq(y.subs(x,T1),p1),
     sympy.Eq(dy.subs(x,T1),dp1)]
-  print(system)
+  print(f"system={system}")
 
   sol=sympy.solve(system,
     [a,b,c,d])
-  print(sol)
+  print(f"solution={sol}")
+  
+  p0_target=0
+  dp0_target=0
+  p1_target=1
+  dp1_target=1 #10 will make the solution non-monotonic
+  T0_target=0
+  T1_target=1
 
-  print(y)
   ysol=y.subs([
     (a,sol[a]),
     (b,sol[b]),
     (c,sol[c]),
-    (d,sol[d])])
-  ynum=ysol.subs([
-    (p0,0),
-    (dp0,0),
-    (p1,1),
-    (dp1,10),
-    (T0,0),
-    (T1,1)])
-  print(ynum)
-  p1=sympy.plot(ynum,xlim=(0,1),ylim=(-1,2),backend="matplotlib")
+    (d,sol[d]),
+    (p0,p0_target),
+    (dp0,dp0_target),
+    (p1,p1_target),
+    (dp1,dp1_target),
+    (T0,T0_target),
+    (T1,T1_target)])
+  print(ysol)
+
+  # Find roots of derivative
+  root_sol=sympy.solve(sympy.Eq(sympy.diff(ysol,x),0),x)
+  print(f"root_sol={root_sol}")
+
+  for xr in root_sol:
+    if xr>T0_target and xr<T1_target:
+      print("non monotonic!")
+
+  # Plot function
+  p1=sympy.plot(ysol,xlim=(0,1),ylim=(-1,2),backend="matplotlib")
 
 if __name__=="__main__":
   fit_polynomial_monotonic()
